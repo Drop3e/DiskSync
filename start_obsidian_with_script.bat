2849e21aded0b9c11ebb5e218bf7aeb14b1b7315
Drop3e <SupraTg85@gmail.com>
2024-11-29 16:03:38 +0200

    script_start.bat

--- start_obsidian_with_script.bat
+++ start_obsidian_with_script.bat
@@ -1,4 +1,4 @@
 @echo off
-start "" "C:\Users\Drop3e\AppData\Local\Programs\Obsidian\Obsidian.exe"
-start "" pythonw "C:\Users\Drop3e\Desktop\DiskSync\main.py"
+start "" "# your global path for programm" 
+start "" pythonw "# your global path for this repository" 
 
