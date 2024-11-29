7ff3ed03f369c2c0901f3b15730b3a442c213d48
Drop3e <SupraTg85@gmail.com>
2024-11-29 15:51:53 +0200

    start_obsidian_with_script.bat

--- start_obsidian_with_script.bat
+++ start_obsidian_with_script.bat
@@ -1,3 +1,4 @@
 @echo off
 start "" "C:\Users\Drop3e\AppData\Local\Programs\Obsidian\Obsidian.exe"
-start "" pythonw "C:\Users\Drop3e\Desktop\Synchronizer files\programs_checker.py"
+start "" pythonw "C:\Users\Drop3e\Desktop\DiskSync\main.py"
+
